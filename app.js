6a082d56c6589158cd6f0f35b1a3d49c9d0ff1bf
Corbin Swagerty <corbin.swagerty@gmail.com>
2017-05-11 13:11:35 -0500

    Added dependencies  - jquery  - underscore  - backbone  - require.js

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
-})
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.use(express.static("app"));
 
 app.listen(3000, function () {
   console.log('Server started at http://localhost:3000');
-})
+});
---
+++ app/main.js
@@ -0,0 +1,5 @@
+require([], function() {
+
+	var collection = new Backbone.Collection([1,2,3]);
+
+});
