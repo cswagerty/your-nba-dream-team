68894b12b21fdfd3007ea92c3925a9991f8c78e5
Corbin Swagerty <corbin.swagerty@gmail.com>
2017-05-15 12:14:23 -0500

    - Stylus files are now compiled into CSS and minified

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function(grunt) {
 
             compile: {
                 files: {
-                    ["app/templates/compiled.js"]: ["app/templates/*.hbs"]
+                    "app/templates/compiled.js": "app/templates/*.hbs"
                 }
             }
         },
@@ -18,29 +18,42 @@ module.exports = function(grunt) {
             compile: {
                 options: {
                     baseUrl: 'app',
-                    include: ['main.js'],
+                    include: "main.js",
                     out: 'dist/main.js'
                 }
             }
         },
 
+        stylus: {
+            compile: {
+                files: {
+                    "dist/main.css": "app/styles/main.styl"
+                }
+            }  
+        },
+
         watch: {
             scripts: {
-                files: ["app/**/*.js"],
-                tasks: ["default"]
+                files: "app/**/*.js",
+                tasks: "requirejs"
             },
 
             templates: {
-                files: ["app/**/*.hbs"],
-                tasks: ["handlebars"]               
+                files: "app/**/*.hbs",
+                tasks: ["handlebars", "requirejs"]
+            },
+            styles: {
+                files: "app/**/*.styl",
+                tasks: "stylus"
             }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-handlebars');
     grunt.loadNpmTasks('grunt-contrib-requirejs');
+    grunt.loadNpmTasks('grunt-contrib-stylus');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
     // A very basic default task.
-    grunt.registerTask('default', ["handlebars", "requirejs", "watch"]);
+    grunt.registerTask('default', ["handlebars", "requirejs", "stylus", "watch"]);
 };
